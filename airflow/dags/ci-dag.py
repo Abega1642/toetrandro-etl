aff364065c433ca9bcf96d3f4a96919a0a35ebf5
Abega1642 <hei.abega.2@gmail.com>
2025-06-17 22:16:20 +0300

    feat: add ci-dag.py

---
+++ airflow/dags/ci-dag.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="ci_dag",
+    start_date=datetime(2025, 6, 17),
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["ci-dag"]
+) as dag:
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+    start >> end
