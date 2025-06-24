519994c00edea42cfabc5cf0349fabce476ad6f3
Abega1642 <hei.abega.2@gmail.com>
2025-06-24 18:45:57 +0300

    feat(src/utils): add City class in city_geo_coordinates/city.py

---
+++ src/utils/city_geo_coordinates/city.py
@@ -0,0 +1,17 @@
+class City:
+    def __init__(self, name):
+        self.name = name
+        self.latitude = None
+        self.longitude = None
+
+    def set_coordinates(self, lat, lon):
+        self.latitude = lat
+        self.longitude = lon
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "lat": self.latitude,
+            "lon": self.longitude
+        }
+
