d76f23317a1db81c7388c187d06541df8037884a
Abega1642 <hei.abega.2@gmail.com>
2025-07-04 11:35:32 +0300

    feat: add city_config step in the etl process

--- workflows/dags/toetrandro_etl.py
+++ workflows/dags/toetrandro_etl.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from workflows.scripts.cities_config_step import CityConfigStep
+
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 if project_root not in sys.path:
@@ -22,6 +24,8 @@ default_args = {
     "retries": 1,
 }
 
+city_list = ["New York", "Paris", "Tokyo", "Toliara", "Mahajanga", "Toamasina"]
+
 with DAG(
     dag_id="toetrandro_etl_pipeline",
     default_args=default_args,
@@ -30,6 +34,9 @@ with DAG(
     max_active_runs=1,
     tags=["toetrandro", "etl"],
 ) as dag:
+
+    def run_city_config():
+        CityConfigStep(city_list).run()
 
     def run_extract():
         ExtractStep().run()
@@ -40,6 +47,12 @@ with DAG(
 
     def run_transform():
         TransformStep().run()
+
+    city_config_task = PythonOperator(
+        task_id="establish_city_config",
+        python_callable=run_city_config,
+        dag=dag,
+    )
 
     extract_task = PythonOperator(
         task_id="extract_weather_data",
@@ -56,4 +69,4 @@ with DAG(
         python_callable=run_merge,
     )
 
-    extract_task >> transform_task >> merge_task
+    city_config_task >> extract_task >> transform_task >> merge_task
